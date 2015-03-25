22f838d6b9f7ef8414223027df789dd4677da687
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-03-25 14:09:23 -0700

    Added download of boot2docker package.

--- install.js
+++ install.js
@@ -5,6 +5,10 @@ var path = require('path');
 
 var BOOT2DOCKER = 'Boot2Docker';
 
+var PROVIDER_URL_V1_4_1 =
+  'https://github.com/boot2docker/osx-installer/releases/download/v1.4.1/' +
+  'Boot2Docker-1.4.1.pkg';
+
 var PROVIDER_URL_PROFILE =
   'https://raw.githubusercontent.com/' +
   'kalabox/kalabox-boot2docker/master/profile';
@@ -18,7 +22,7 @@ module.exports = function(kbox) {
     step.name = 'is-boot2docker-installed';
     step.description = 'Check if boot2docker is installed.';
     step.deps = [];
-    step.all = function(state, done) {
+    step.all.darwin = function(state, done) {
       sysProfiler.isAppInstalled(BOOT2DOCKER, function(err, isInstalled) {
         if (err) {
           done(err);
@@ -49,7 +53,11 @@ module.exports = function(kbox) {
   kbox.install.registerStep(function(step) {
     step.name = 'download-docker-dependencies';
     step.description = 'Download docker dependencies';
-    step.deps = ['internet'];
+    step.deps = [
+      'is-boot2docker-installed',
+      'is-boot2docker-profile-set',
+      'internet'
+    ];
     step.all.darwin = function(state, done) {
 
       // Init.
@@ -63,8 +71,16 @@ module.exports = function(kbox) {
         state.dockerDependencyDownloads.push(PROVIDER_URL_PROFILE);
       }
 
+      // Boot2docker package.
+      if (!state.isBoot2DockerInstalled) {
+        state.dockerDependencyDownloads.push(PROVIDER_URL_V1_4_1);
+      }
+
       // Download dependencies/
       var urls = state.dockerDependencyDownloads;
+      urls.forEach(function(url) {
+        state.log(url);
+      });
       if (urls.length > 0) {
         kbox.util.download.downloadFiles(urls, state.downloadDir, function() {
           done();          
