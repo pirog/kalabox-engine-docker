0e687e89c2dcdedf4860b8bb82fd374849e093db
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-03-25 12:06:15 -0700

    Added a few small installer steps.

--- install.js
+++ install.js
@@ -1,13 +1,42 @@
 'use strict';
 
+var fs = require('fs');
+
+var BOOT2DOCKER = 'Boot2Docker';
+
 module.exports = function(kbox) {
 
+  var sysProfiler = kbox.install.sysProfiler;
+
+  // Boot2docker installed?
   kbox.install.registerStep(function(step) {
-    step.name = 'docker';
-    step.description = step.name;
+    step.name = 'boot2docker-installed';
+    step.description = 'Check if boot2docker is installed.';
     step.deps = [];
-    step.all = function(state) {
-      console.log(step.name);
+    step.all = function(state, done) {
+      sysProfiler.isAppInstalled(BOOT2DOCKER, function(err, isInstalled) {
+        if (err) {
+          done(err);
+        } else {
+          state.isBoot2DockerInstalled = isInstalled;
+          state.log(BOOT2DOCKER + ' is installed?: ' + isInstalled);
+          done();
+        }
+      });
+    };
+  });
+
+  // Boot2docker profile exists?
+  kbox.install.registerStep(function(step) {
+    step.name = 'boot2docker-profile-exists';
+    step.description = 'Check if boot2docker profile exists.';
+    step.deps = [];
+    step.all.darwin = function(state, done) {
+      var filepath = path.join(config.sysProviderRoot, 'profile');
+      fs.exists(filepath, function(exists) {
+        state.boot2dockerProfileExists = exists;
+        done();
+      });
     };
   });
 
