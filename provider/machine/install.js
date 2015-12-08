75165f93f127944c60d211d51182011d6b2d15f0
Mike Pirog <mike@kalamuna.com>
2015-12-07 16:35:49 -0800

    #790: I guess this is implied

--- provider/machine/install.js
+++ provider/machine/install.js
@@ -175,7 +175,7 @@ module.exports = function(kbox) {
           state.adminCommands.push(extractCmd);
 
           // Build the install command
-          var vbInstallOptions = ['/qn', '/passive', '/quiet'];
+          var vbInstallOptions = ['/qn', '/passive'];
           // Split into parts VIRTUALBOX|VERSION|RELEASE|ARCH
           var pts = vb.split('-');
           var msiName = [pts[0], pts[1], 'r' + pts[2], 'MultiArch_amd64.msi'];
